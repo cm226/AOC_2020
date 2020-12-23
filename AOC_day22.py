9c1fcadcce054e9e26b1cccf5857e51f19bca455
Craig <c.matear@gmail.com>
2020-12-23 14:36:48 +0000

    part two day 23

--- AOC_day22.py
+++ AOC_day22.py
@@ -1,3 +1,4 @@
+from copy import copy
 f = open("input_day22.txt")
 
 player1 = []
@@ -16,16 +17,41 @@ for l in f:
 
 # sim game
 def simulateGame(player1, player2):
+    roundHistory = {}
+
     while len(player1) > 0 and len(player2) > 0:
+        p1str = str(player1)
+        p2str = str(player2)
+        stateHash = p1str + ',' + p2str
+
+        if stateHash in roundHistory:
+            return 'p1', player1
+        
+        roundHistory[stateHash] = ''
+
         c1 = player1.pop(0)
         c2 = player2.pop(0)
 
-        if c1>c2:
-            player1.extend([c1, c2])
-        else:
-            player2.extend([c2, c1])
+        subWinner = None
+        if len(player1) >= c1 and len(player2) >= c2:
+            subWinner, deck = simulateGame(copy(player1[0:c1]), copy(player2[0:c2]))
 
-    winner = player2 if len(player2)!=0 else player1
+        if subWinner != None:
+            if subWinner == 'p1':
+                player1.extend([c1,c2])
+            else:
+                player2.extend([c2,c1])
+        else:
+            if c1>c2:
+                player1.extend([c1, c2])
+            else:
+                player2.extend([c2, c1])
+
+    if len(player2)!=0:
+        return 'p2', player2
+    else:
+        return 'p1', player1
+
     
 def calcScore(winner):
     # calc score
@@ -36,3 +62,9 @@ def calcScore(winner):
         i+=1
 
     print(score)
+
+winner, deck = simulateGame(player1, player2)
+
+print(winner, deck)
+
+calcScore(deck)
--- AOC_day23.py
+++ AOC_day23.py
@@ -1,57 +1,113 @@
+import math
+
+def printList(l):
+    start = l
+    cur = start
+    while cur != start:
+        print(cur.val, sep=',', end='')
+        cur = cur.next
+
+class Cup:
+
+    next = None
+    val = None
+    def __init__(self, val):
+        self.next = None
+        self.val = val
+
+
 
 input="916438275"
-cups = list(input)
-curCupIdx = 0
-
-cups = list(map(int, cups))
+cuplist = list(input)
+cuplist = list(map(int, cuplist))
 
 # find min and max cup vals
-lims = [max(cups),min(cups)] 
+lims = [max(cuplist),min(cuplist)] 
+cuplist.extend(range(lims[0]+1, 1000001))
+# adjust limits after extending
+lims[0] = 1000000 
 
-for i in range(100):
-    grabedCups = []
-    curCupVal = cups[curCupIdx]
+cups = Cup(cuplist[0])
+cupMap = {}
+cupMap[cuplist[0]] = cups
+last = cups
+for c in cuplist[1:]:
+    last.next = Cup(c)
+    last = last.next
+    cupMap[c] = last
 
-    for j in range(curCupIdx+1,curCupIdx+4):
-        grabedCups.append( cups[(j) % len(cups)] )
+last.next = cups
 
-    # remove the grabed cups
-    cups = [c for c in cups if c not in grabedCups]
-    # index is prob no longer correct so find it again
-    for i in range(len(cups)):
-        if cups[i] == curCupVal:
-            curCupIdx = i
-            break
+curCup = cups
+
+
+lastReported = 0
+
+# add the other cups 
+
+
+#for i in range(100):
+for i in range(10000000):
     
+    #print("cups")
+    #printList(curCup)
 
+    grabedCups = curCup.next
+
+    #print("grabed")
+    #printList(grabedCups)
+
+    lastGrabed = curCup
+    for i in range(3):
+        lastGrabed = lastGrabed.next
+    
+    curCup.next = lastGrabed.next
+    lastGrabed.next = None
+    
+    
     #find dest
     dest = None
-    val = cups[curCupIdx]
-    while dest == None:
+    val = curCup.val
+
+    while True:
         val -= 1
         val = lims[0] if val < lims[1] else val
-        for i in range(len(cups)):
-            if(cups[i] == val):
-                dest = i
+
+        cur_grabed = grabedCups
+        inGrabed = False
+        while cur_grabed != lastGrabed.next:
+            if cur_grabed.val == val:
+                inGrabed = True
                 break
-
-    for j in range(len(grabedCups)):
-        cups.insert(dest+j+1, grabedCups[j])
-
-
-    # index is prob no longer correct so find it again
-    for i in range(len(cups)):
-        if cups[i] == curCupVal:
-            curCupIdx = i
+            cur_grabed = cur_grabed.next
+        if not inGrabed:
             break
-    #print(cups)
-    curCupIdx= (curCupIdx+1) % len(cups)
 
 
-for i in range(len(cups)):
-    if cups[i] == 1:
-        for j in range(1,len(cups)):
-            print(cups[(i+j) % len(cups)], sep='', end='')
-        break
+    dest = cupMap[val]
+
+    tmp = dest.next
+    dest.next = grabedCups
+
+    lastGrabed.next = tmp
+
+    curCup = curCup.next
+
+    progress = math.floor((i / 1000000.0)*100.0)
+    if progress > lastReported:
+        print(progress)
+        lastReported = progress
+
+#print(cups)
+
+# startCup = cupMap[1].next
+# for j in range(1,len(cupMap)):
+#    print(startCup.val, sep='', end='')
+#    startCup = startCup.next
+        
+print()
+print(cupMap[1].next.val)
+print(cupMap[1].next.next.val)
+print(cupMap[1].next.val*cupMap[1].next.next.val)
 
     
